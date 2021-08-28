efbd871aff7097216e388ba65d761021454b1c53
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-08-27 23:45:39 -0300

    lateral pt2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './pages/Home'
 import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset'; 
 import theme from './theme';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <Reset />
         <Home />
       </ThemeProvider>
     </>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    background-color: blue;
+export const Container = styled.aside`
+    background-color: ${(props) => props.theme.colors.background};
+    width: 350px;
+    height: 100vh;
+    overflow-y: auto;
 ` 
